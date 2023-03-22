82e8b62526164884c928cc346476c4ffeaa1aae3
Benisha <benishanelson100@gmail.com>
2023-03-22 10:37:33 -0230

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.querySelector('#push').onclick=function(){
+//   if(document.querySelector('#newTask input').value.length==0){
+//     alert('Please Enter your plan')
+//   }
+//   else{
+//     document.querySelector('#tasks').innerHTML=
+//     $(document.querySelector('#newTask input').value);
+//  var currentTask=document.querySelectorAll(".delete");
+// for(var i=0;i<currentTask.length;i++){
+// currentTask[i].onclick=function(){
+// this.parentNode.remove();
+// }
+// }
+// }
+// }
